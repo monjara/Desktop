33abe1a42efbb7494b6a543d2237ddb363aa937b
monjara <yaginumee@gmail.com>
2022-04-10 01:04:36 +0900

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const urlPrefix = process.env.NODE_ENV === "production" ? "/Desktop" : ""
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,19 +8,19 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'ubuntu_by_arman1992': "url('/backgrounds/ubuntu_by_arman1992.jpg')",
-        'ubuntu-default-greyscale-wallpaper': "url('/backgrounds/ubuntu-default-greyscale-wallpaper.png')",
-        'ubuntu2_by_arman1992': "url('/backgrounds/ubuntu2_by_arman1992.jpg')",
-        'Blue_flower_by_Elena_Stravoravdi': "url('/backgrounds/Blue_flower_by_Elena_Stravoravdi.jpg')",
-        'Cherry_Tree_in_Lakones_by_elenastravoravdi': "url('/backgrounds/Cherry_Tree_in_Lakones_by_elenastravoravdi.jpg')",
-        'DSC2943_by_kcpru': "url('/backgrounds/DSC2943_by_kcpru.jpg')",
-        'Jammy-Jellyfish_WP_4096x2304_Grey': "url('/backgrounds/Jammy-Jellyfish_WP_4096x2304_Grey.png')",
-        'Mirror_by_Uday_Nakade': "url('/backgrounds/Mirror_by_Uday_Nakade.jpg')",
-        'Optical_Fibers_in_Dark_by_Elena_Stravoravdi': "url('/backgrounds/Optical_Fibers_in_Dark_by_Elena_Stravoravdi.jpg')",
-        'canvas_by_roytanck': "url('/backgrounds/canvas_by_roytanck.jpg')",
-        'jj_dark_by_Hiking93': "url('/backgrounds/jj_dark_by_Hiking93.jpg')",
-        'jj_light_by_Hiking93': "url('/backgrounds/jj_light_by_Hiking93.jpg')",
-        'warty-final-ubuntu': "url('/backgrounds/warty-final-ubuntu.png')",
+        'ubuntu_by_arman1992': `url('${urlPrefix}/backgrounds/ubuntu_by_arman1992.jpg')`,
+        'ubuntu-default-greyscale-wallpaper': `url('${urlPrefix}/backgrounds/ubuntu-default-greyscale-wallpaper.png')`,
+        'ubuntu2_by_arman1992': `url('${urlPrefix}/backgrounds/ubuntu2_by_arman1992.jpg')`,
+        'Blue_flower_by_Elena_Stravoravdi': `url('${urlPrefix}/backgrounds/Blue_flower_by_Elena_Stravoravdi.jpg')`,
+        'Cherry_Tree_in_Lakones_by_elenastravoravdi': `url('${urlPrefix}/backgrounds/Cherry_Tree_in_Lakones_by_elenastravoravdi.jpg')`,
+        'DSC2943_by_kcpru': `url('${urlPrefix}/backgrounds/DSC2943_by_kcpru.jpg')`,
+        'Jammy-Jellyfish_WP_4096x2304_Grey': `url('${urlPrefix}/backgrounds/Jammy-Jellyfish_WP_4096x2304_Grey.png')`,
+        'Mirror_by_Uday_Nakade': `url('${urlPrefix}/backgrounds/Mirror_by_Uday_Nakade.jpg')`,
+        'Optical_Fibers_in_Dark_by_Elena_Stravoravdi': `url('${urlPrefix}/backgrounds/Optical_Fibers_in_Dark_by_Elena_Stravoravdi.jpg')`,
+        'canvas_by_roytanck': `url('${urlPrefix}/backgrounds/canvas_by_roytanck.jpg')`,
+        'jj_dark_by_Hiking93': `url('${urlPrefix}/backgrounds/jj_dark_by_Hiking93.jpg')`,
+        'jj_light_by_Hiking93': `url('${urlPrefix}/backgrounds/jj_light_by_Hiking93.jpg')`,
+        'warty-final-ubuntu': `url('${urlPrefix}/backgrounds/warty-final-ubuntu.png')`,
       }),
     },
   },
