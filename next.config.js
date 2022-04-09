1ab60f25d9dd74488e600c1c43c8785b0870bb5c
monjara <yaginumee@gmail.com>
2022-04-10 00:25:16 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "Desktop" : ""
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Desktop" : ""
 }
 
 module.exports = nextConfig
