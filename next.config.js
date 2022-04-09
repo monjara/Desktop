f9da70452213184051b6d9bd9ee7645616e62e7d
monjara <yaginumee@gmail.com>
2022-04-10 00:43:40 +0900

    add config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Desktop" : ""
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Desktop" : "",
 }
 
 module.exports = nextConfig
