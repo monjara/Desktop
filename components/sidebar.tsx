b6497cb77a834497863b7d59de84006d844bd882
monjara <yaginumee@gmail.com>
2022-04-10 14:09:32 +0900

    convert to img from next/image

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image'
-
 const Sidebar = () => {
   return (
     <div className='w-24 bg-black opacity-50 absolute top-9 bottom-0 left-0'>
       <div className='w-full h-24 hover:bg-white rounded-2xl'>
-        <Image
+        <img
           src='/icons/com.visualstudio.code.png'
           alt='vscode'
           width='90%'
