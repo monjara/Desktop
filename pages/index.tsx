5dde7ab37ef80bdf942cd798494672a931887ba7
monjara <yaginumee@gmail.com>
2022-04-12 02:45:04 +0900

    fix meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Desktop from '../components/desktop'
 
 const Home: NextPage = () => {
+  const urlPrefix = process.env.NODE_ENV === 'production' ? '/Desktop' : ''
   return (
-    <div className='hidden-scrollbar'>
-      <Desktop />
-    </div>
+    <>
+      <Head>
+        <title>Desktop</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <link rel='icon' href={`${urlPrefix}/favicon.ico`} />
+      </Head>
+        <Desktop />
+    </>
   )
 }
 
