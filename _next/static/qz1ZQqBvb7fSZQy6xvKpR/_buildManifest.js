23ec14d2c57cb6ec4b916880350baf59fc1b8bfa
monjara <monjara@users.noreply.github.com>
2022-04-11 16:02:10 +0000

    deploy: monjara/Desktop@d05a806e6daa050033c07c996d7e6380c2246dca

--- _next/static/qz1ZQqBvb7fSZQy6xvKpR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/461-396c4b8156bae976.js","static/chunks/pages/index-b399a6a3b2dc1887.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
